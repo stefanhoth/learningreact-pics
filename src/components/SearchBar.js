2892f9ad45b90a15769a14385655a2c66571904e
Stefan Hoth <45467+stefanhoth@users.noreply.github.com>
2021-12-26 21:49:02 +0100

    feat: SearchBar is now controlled element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' };
 
   render() {
     return (
@@ -11,7 +9,12 @@ export default class SearchBar extends Component {
         <form className="ui form">
           <div className="field">
             <label htmlFor="query">Image search</label>
-            <input type="text" id="query" onChange={this.onInputChange} />
+            <input
+              type="text"
+              id="query"
+              onChange={(e) => this.setState({ term: e.target.value })}
+              value={this.state.term}
+            />
           </div>
         </form>
       </div>
