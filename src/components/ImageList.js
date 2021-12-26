a4fd2cee1f27598e9d38dc47d8fdbcc4d125971a
Stefan Hoth <45467+stefanhoth@users.noreply.github.com>
2021-12-27 00:10:37 +0100

    feat: Use cSS grid for layout of results

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,6 +1,8 @@
+import "./ImageList.css";
+
 const ImageList = (props) => {
-  const images = props.images.map(({urls, alt_description, id}) => {
-    return <img src={urls.small} alt={alt_description} key={'unsplash-' + id} />;
+  const images = props.images.map(({ urls, alt_description, id }) => {
+    return <img src={urls.small} alt={alt_description} key={id} />;
   });
 
   return <div className="image-list">{images}</div>;
