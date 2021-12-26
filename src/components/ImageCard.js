b19cbf97335f94f2dbb9c9dd4b9216c887b2b18d
Stefan Hoth <45467+stefanhoth@users.noreply.github.com>
2021-12-27 00:52:38 +0100

    feat: Render image card with adjusted grid space

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react";
 
 export class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 20); // see ImageList.css grid-auto-rows: 20px
+    this.setState({ spans }); //same as {spans: spans}
+  };
+
   render() {
     const { urls, alt_description } = this.props.image;
 
     return (
-      <div className="image-card">
-        <img src={urls.small} alt={alt_description} />;
+      <div
+        className="image-card"
+        style={{ gridRowEnd: `span ${this.state.spans}` }}
+      >
+        <img ref={this.imageRef} src={urls.small} alt={alt_description} />
       </div>
     );
   }
